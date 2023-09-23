6bc42b943c742ae89c3d4398a0782e4a1c3752bf
ashim120303 <ashim030312@gmail.com>
2023-09-23 19:58:29 +0600

    Start project.

---
+++ src/test/java/com/pack/onetrust/OneTrustApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pack.onetrust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneTrustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
